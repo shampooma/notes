49bda1b90704da0870d0267965bcc80efedf1d63
shampoo Ma <a0017452406@gmail.com>
2022-01-16 15:07:34 +0800

    first try

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,25 @@
+const path = require("path")
+const fs = require("fs")
+
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        // {
+        //     resolve: `gatsby-plugin-manifest`,
+        //     options: {
+        //       name: `GatsbyJS`,
+        //       short_name: `GatsbyJS`,
+        //       start_url: `/`,
+        //       background_color: `#f7f0eb`,
+        //       theme_color: `#a2466c`,
+        //       display: `standalone`,
+        //       icon: `src/images/icon.png`,
+        //     },
+        //   },
+        //   {
+        //     resolve: `gatsby-plugin-offline`,
+        //   },
     ]
 }
